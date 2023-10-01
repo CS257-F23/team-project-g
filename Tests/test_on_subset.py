456fef074501455df07eaf799471dd0212d8998e
Lee <lee@Lees-Mac.local>
2023-09-30 23:55:50 -0500

    first iteration of test_get_restriction()

--- Tests/test_on_subset.py
+++ Tests/test_on_subset.py
@@ -4,6 +4,7 @@ import unittest
 
 class Test_command_line(unittest.TestCase):
     #testing get_cell (basic test)
+    load_subset_data()
     def test_get_restriction_one_food(self):
         """Check if get_restriction() works for a list with one valid food item"""
         food = ["Crispy Bell Peppers"]
@@ -23,12 +24,45 @@ class Test_command_line(unittest.TestCase):
         self.assertCountEqual(get_restriction(food),expected)
 
     def test_get_restriction_no_food(self):
+        """Check if get_restriction() raises ValueError for an argument of empty list"""
         food = []
         self.assertRaises(ValueError, get_restriction, food)
-
     
-        
+    def test_get_restriction_wrong_food_name(self):
+        """Check if get_restriction() raises ValueError for an argument of list containing a food item that does not exists in dataset"""
+        food = ["Silly"]
+        self.assertRaises(ValueError, get_restriction, food)
+    
+    def test_get_restriction_wrong_arg_type(self):
+        """Check if get_restriction() raises TypeError for an argument of string"""
+        food = "Crispy Bell Peppers"
+        self.assertRaises(TypeError, get_restriction, food)
 
+    def test_get_restriction_main_basic(self):
+        """Check if ____.py works for valid command line arguments"""
+        food1 = "'Crispy Bell Peppers'"
+        food2 = "'Garden Herb Ranch Dressing'"
+        option = "-diet"
+        expected = "['Dairy','Egg', 'Wheat']"
+        file_path = 'ProductionCode/____.py' #path to the production code
+        code = subprocess.Popen(['python3', file_path, option, food1, food2], 
+                                stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                encoding='utf8') 
+        output, err = code.communicate() 
+        self.assertEqual(output.strip(), expected) 
+        code.terminate() 
+
+    def test_get_restriction_main_basic(self):
+        """Check if basic_cl.py works for invalid command line arguments"""
+        option = "-diet"
+        expected = "Usage : python3 ____.py -diet 'food1' 'food2'"
+        file_path = 'ProductionCode/____.py' #path to the production code
+        code = subprocess.Popen(['python3', file_path, option], 
+                                stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                encoding='utf8') 
+        output, err = code.communicate() 
+        self.assertEqual(output.strip(), expected) 
+        code.terminate() 
     
         
 if __name__ == '__main__':
