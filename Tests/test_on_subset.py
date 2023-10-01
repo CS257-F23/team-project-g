bab6ca44b41bae5e5b327de1b2b51cd4a28694f7
Lee <lee@Lees-Mac.local>
2023-09-30 23:09:37 -0500

    add test for get_restriction() and upload subset of dataset

---
+++ Tests/test_on_subset.py
@@ -0,0 +1,33 @@
+import subprocess
+import unittest
+#from ProductionCode import *
+
+class Test_command_line(unittest.TestCase):
+    #testing get_cell (basic test)
+    def test_get_restriction_one_food(self):
+        """Check if get_restriction() works for a list with one valid food item"""
+        food = ["Crispy Bell Peppers"]
+        expected = ["Wheat"]
+        self.assertCountEqual(get_restriction(food),expected)
+    
+    def test_get_restriction_two_food(self):
+        """Check if get_restriction() works for a list with two valid food items"""
+        food = ["Crispy Bell Peppers", "Garden Herb Ranch Dressing"]
+        expected = ["Dairy","Egg", "Wheat"]
+        self.assertCountEqual(get_restriction(food),expected)
+
+    def test_get_restriction_five_food(self):
+        """Check if get_restriction() works for a list with five valid food items"""
+        food = ["Crispy Bell Peppers", "Garden Herb Ranch Dressing","Roasted Nut Blend", "5 Ct Nuggets Kid's Meal", "Tomato"]
+        expected = ["Dairy","Egg", "Wheat", "Tree Nuts"]
+        self.assertCountEqual(get_restriction(food),expected)
+
+    def test_get_restriction_no_food(self):
+        
+    
+        
+
+    
+        
+if __name__ == '__main__':
+    unittest.main()
