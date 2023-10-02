1a5731a7080795e21b06fad92cf2843c0785b1fd
Hank Zhao <hzhao@slingwave.com>
2023-10-02 18:16:34 -0500

    10/2 version 2

--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -6,17 +6,17 @@ class Test_get_calories_by_name(unittest.TestCase):
 
     
     def test_get_calories_by_name_allcorrect1(self):
-        """Check if get_calories_by_name works for valid food item."""
+        """Purpose: Check if get_calories_by_name works for valid food item."""
         calories=get_calories_by_name('Coffee')
         self.assertEqual(calories, '0')
     
     def test_get_calories_by_name_capitalerror(self):
-        """Check if get_calories_by_name() works for valid food item with wrong capitalizaton."""
+        """Purpose: Check if get_calories_by_name() works for valid food item with wrong capitalizaton."""
         calories=get_calories_by_name('PEAch Milkshake')
         self.assertEqual(calories, '590')
     
     def test_get_calories_by_name_wrongfood(self):
-        """Check if get_calories_by_name() output warning for invalid food item."""
+        """Purpose: Check if get_calories_by_name() output warning for invalid food item."""
         food = "Charlie"
         file_path = 'ProductionCode/basic_cl.py' #path to the production code
         code = subprocess.Popen(['python3', file_path,'-calories', food], 
@@ -29,7 +29,7 @@ class Test_get_calories_by_name(unittest.TestCase):
 
         
     def test_get_calories_by_name_commandcorrect(self):
-        """Check if basic_cl.py works for valid 'calories' command line arguments."""
+        """Purpose: Check if basic_cl.py works for valid 'calories' command line arguments."""
         file_path = 'ProductionCode/basic_cl.py' #path to the production code
         code = subprocess.Popen(['python3', file_path,'-calories', 'Coffee'], 
                                 stdin=subprocess.PIPE, stdout=subprocess.PIPE,
@@ -39,7 +39,7 @@ class Test_get_calories_by_name(unittest.TestCase):
         code.terminate()
     
     def test_get_calories_by_name_commanderror(self):
-        """Check if basic_cl.py works for invalid '-calories' command line arguments"""
+        """Purpose: Check if basic_cl.py works for invalid '-calories' command line arguments"""
         file_path = 'ProductionCode/basic_cl.py' #path to the production code
         code = subprocess.Popen(['python3', file_path, '-calories'], 
                                 stdin=subprocess.PIPE, stdout=subprocess.PIPE,
@@ -49,7 +49,7 @@ class Test_get_calories_by_name(unittest.TestCase):
         code.terminate()
     
     def test_commanderror(self):
-        """Check if basic_cl.py works for invalid command line arguments"""
+        """Purpose: Check if basic_cl.py works for invalid command line arguments"""
         file_path = 'ProductionCode/basic_cl.py' #path to the production code
         expected = ("usage method not found, please use one of the usage method below: \n"
            "Usage: python3 ProductionCode/basic_cl.py -calories 'food'\n"
--- Tests/test_on_subset.py
+++ Tests/test_on_subset.py
@@ -6,25 +6,25 @@ class Test_get_restriction(unittest.TestCase):
     #testing get_cell (basic test)
     load_data_subset()
     def test_get_restriction_one_food(self):
-        """Check if get_restriction() works for a list with one valid food item"""
+        """Purpose: Check if get_restriction() works for a list with one valid food item"""
         food = ["Crispy Bell Peppers"]
         expected = ["Wheat"]
         self.assertCountEqual(get_restriction(food),expected)
     
     def test_get_restriction_two_food(self):
-        """Check if get_restriction() works for a list with two valid food items"""
+        """Purpose: Check if get_restriction() works for a list with two valid food items"""
         food = ["Crispy Bell Peppers", "Garden Herb Ranch Dressing"]
         expected = ["Dairy", "Egg", "Wheat"]
         self.assertCountEqual(get_restriction(food),expected)
 
     def test_get_restriction_five_food(self):
-        """Check if get_restriction() works for a list with five valid food items"""
+        """Purpose: Check if get_restriction() works for a list with five valid food items"""
         food = ["Crispy Bell Peppers", "Garden Herb Ranch Dressing","Roasted Nut Blend", "5 Ct Nuggets Kid's Meal", "Tomato"]
         expected = ["Dairy", "Egg", "Wheat", "Tree Nuts"]
         self.assertCountEqual(get_restriction(food),expected)
 
     def test_get_restriction_main_invalid_food(self):
-        """Check if basic_cl.py works for an argument of list containing a food item that does not exists in datasets"""
+        """Purpose: Check if basic_cl.py works for an argument of list containing a food item that does not exists in datasets"""
         option = "-diet"
         food = "Silly"
         expected = "Usage : python3 Production/basic_cl.py -diet 'food1' 'food2'"
@@ -37,7 +37,7 @@ class Test_get_restriction(unittest.TestCase):
         code.terminate() 
 
     def test_get_restriction_main_basic(self):
-        """Check if basic_cl.py works for valid command line arguments"""
+        """Purpose: Check if basic_cl.py works for valid command line arguments"""
         food1 = "Crispy Bell Peppers"
         food2 = "Garden Herb Ranch Dressing"
         option = "-diet"
@@ -51,7 +51,7 @@ class Test_get_restriction(unittest.TestCase):
         code.terminate() 
 
     def test_get_restriction_main_invalid(self):
-        """Check if basic_cl.py works for invalid command line arguments"""
+        """Purpose: Check if basic_cl.py works for invalid command line arguments"""
         option = "-diet"
         expected = "Usage : python3 Production/basic_cl.py -diet 'food1' 'food2'"
         file_path = 'ProductionCode/basic_cl.py' #path to the production code
