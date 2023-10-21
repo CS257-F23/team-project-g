384b05d04026180d6fc999087b31aac4bf80fb94
YumingZhang2004 <aphel1os@Aphel1os>
2023-10-21 16:18:21 -0500

    GD3 updated

--- app_new.py
+++ app_new.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
      '''renders html for the homepage'''
-     return render_template("homepage.html")
+     return render_template("homepage_gd3.html")
 
 @app.route('/get_calorie', strict_slashes=False)
 def get_calorie_by_food():
@@ -18,7 +18,7 @@ def get_calorie_by_food():
      if calorie == "Sorry, the item you are searching for is not in the menu of Chick-fil-A.":
           return render_template("food_not_found_page.html")
      calorie = int(calorie)
-     return render_template("calorie_page.html", food = food, count = calorie, num_present = round(100*(calorie/600), 2))
+     return render_template("calorie_page_gd3.html", food = food, count = calorie, num_present = round(100*(calorie/600), 2))
 
 @app.route('/get_diet', strict_slashes=False)
 def get_diet():
@@ -28,13 +28,13 @@ def get_diet():
      food_list = food.split(",")
      allergies = str(cl.get_restriction(food_list))
      if allergies == "Sorry, the item you are searching for is not in the menu of Chick-fil-A.":
-          return render_template("food_not_found_page.html")
-     return render_template("diet_page.html", food = food, allergies = allergies)
+          return render_template("food_not_found_page_gd3.html")
+     return render_template("diet_page_gd3.html", food = food, allergies = allergies)
 
 @app.errorhandler(404)
 def page_not_found(e):
      '''error handling for 404 error (URL not found)'''
-     return render_template("404.html")
+     return render_template("errorpage_gd3.html")
 
 @app.errorhandler(500)
 def python_bug(e):
