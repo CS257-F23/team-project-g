d55ed6d192150deb7fa4272e9f6aed062929e42b
Hank Zhao <hzhao@slingwave.com>
2023-11-07 16:43:39 -0600

    gd4

---
+++ Data/createtable.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS food_info;
+CREATE TABLE food_info(
+  food text,
+  calories int,
+  dairy boolean,
+  egg boolean,
+  soy boolean,
+  wheat boolean,
+  nuts boolean,
+  fish boolean
+);
---
+++ Tests/test_flask.py
@@ -0,0 +1,62 @@
+from app import *  
+import unittest
+
+class Test_flask_html(unittest.TestCase):
+    
+    #base case
+    def test_home_page(self):
+        '''check if app.py works for the homepage (route '/') '''
+        self.app = app.test_client()
+        route = '/'
+        response = self.app.get(route, follow_redirects=True)
+        expected_pt = "Chick-fil-A Nutrition Guide"
+        self.assertIn(expected_pt, response.data.decode('utf-8'))
+
+    #base case
+    def test_calorie_route(self):
+        '''check if app.py works for valid route for /calorie'''
+        self.app = app.test_client()
+        route = '/get_calorie?option_select=Calorie&text_box=Hash+Browns&search_button=Search+🔍'
+        response = self.app.get(route, follow_redirects=True)
+        expected = "270"
+        self.assertIn(expected, response.data.decode('utf-8'))
+
+    #edge case
+    def test_calorie_route_invalid(self):
+        '''check if app.py works for valid route /calorie but invalid argument()'''
+        self.app = app.test_client()
+        route = '/get_calorie?option_select=Calorie&text_box=Silly&search_button=Search+🔍'
+        response = self.app.get(route, follow_redirects=True)
+        expected = "Sorry"
+        self.assertIn(expected, response.data.decode('utf-8'))
+
+    #edge case
+    def test_calorie_invalid(self): #test for 404 error
+        '''check if app.py works for invalid (non-existing) route, 404 page'''
+        self.maxDiff = None
+        self.app = app.test_client()
+        route = '/calories/Hash Browns'
+        response = self.app.get(route, follow_redirects=True)
+        expected = "Oops"
+        self.assertIn(expected, response.data.decode('utf-8'))
+        
+    #base case
+    def test_diet_route_valid(self):
+        '''check if app.py works for valid route and argument for /diet'''
+        self.app = app.test_client()
+        route = '/get_diet?option_select=Allergy&text_box=Hash+Browns%2CTomato%2CGarden+Herb+Ranch+Sauce&search_button=Search+🔍'
+        response = self.app.get(route, follow_redirects=True)
+        expected_pt = "dairy"
+        self.assertIn(expected_pt, response.data.decode('utf-8'))
+
+    #edge case
+    def test_diet_route_invalid(self):
+        '''check if app.py works for valid route /diet but invalid argument()'''
+        self.app = app.test_client()
+        route = '/get_diet?option_select=Allergy&text_box=Silly&search_button=Search+🔍'
+        response = self.app.get(route, follow_redirects=True)
+        expected = "Sorry"
+        self.assertIn(expected, response.data.decode('utf-8'))
+
+if __name__=="__main__":
+    unittest.main()
